24d5b82a8f038c1cdfe46620ef31ff16bb49b1fa
sullyham <himynameissullyham@gmail.com>
2025-07-16 18:28:15 -0400

    Working on 3SUM

--- src/Summer.java
+++ src/Summer.java
@@ -32,14 +32,33 @@ public class Summer {
             System.out.println(arr[index1] + " + " + arr[index2] + " == " + target);
         }
     }
-    public List<List<Integer>> threeSum(int[] nums){
+    public static List<List<Integer>> threeSum(int[] nums){
         List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int target = -nums[i];
+            int left = 1;
+            int right = nums.length -1;
+            while(left > right){
+                if(nums[i] == nums[left]){
+
+                }
+            }
+        }
         return list;
     }
     public static void main(String[] args) {
-        int[] arr = {-10, -5, -3, 0, 0, 3, 3, 5, 7, 9, 15};
+        int[] arr = {-1,0,1,2,-1,-4};
         Arrays.sort(arr);
-        Sum(0, arr);
+        List<List<Integer>> list = threeSum(arr);
+        for (int i = 0; i < list.size() -1 ; i++) {
+            List<Integer> list1 = list.get(i);
+            for (int j = 0; j < list1.size(); j++) {
+                if(j == list1.size()-1){
+                    System.out.println();
+                }
+                System.out.print(list1.get(j) + " ");
+            }
+        }
 
     }
 }
