425672c802d9b17457d1736ec2da466dd3a63032
sullyham <himynameissullyham@gmail.com>
2025-07-29 14:08:11 -0400

    Following neetcode solution as improvised solution was very off, progress tho!

--- src/Summer.java
+++ src/Summer.java
@@ -15,10 +15,80 @@ public class Summer {
         }
         return max;
     }
-        public static void main(String[] args) {
-        int[] area = {1,8,6,2,5,4,8,3,7};
-       System.out.println(maxArea(area));
+    public static int MAXAREA(int[] heights){
+        int distance = 0;
+        int max = 0;
+        int left = 0;
+        int right = heights.length - 1;
+        int small = 0;
+        while(left < right){
+            small = Math.min(heights[left],heights[right]);
+            distance = small *(right - left);
+            if(distance > max){
+                max = distance;
+            }
+            if(heights[left] > heights[right]){
+                right--;
+            }
+            else{
+                left++;
+            }
 
+        }
+        return max;
+    }
+    public static void trap(int height[]){
+        int[] leftmax = new int[height.length];
+        int[] rightmax = new int[height.length];
+        int max = 0;
+        for (int i = 0; i < height.length; i++) {
+            if(i == 0){
+                leftmax[0] = max;
+            }
+            else{
+                leftmax[i] = max;
+                if(height[i] > max){
+                    max = height[i];
+                }
+
+            }
+
+        }
+        max = 0;
+        for (int i = height.length -1; i >= 0; i--) {
+            if(i == height.length){
+                rightmax[height.length - 1] = 0;
+                max = height[height.length-1];
+            }
+            else{
+                rightmax[i] = max;
+            }
+                if(height[i] > max){
+                    max = height[i];
+                }
+
+
+        }
+        System.out.println("LEFTMAX:");
+        for(int numbers: leftmax){
+            System.out.print(numbers + " ");
+        }
+        System.out.println();
+        System.out.println("RIGHTMAX:");
+        for(int numbers: rightmax){
+            System.out.print(numbers + " ");
+        }
+
+
+    }
+        public static void main(String[] args) {
+        int[] area = {0,1,0,2,1,0,1,3,2,1,2,1};
+       trap(area);
+       System.out.println();
+       System.out.println("OG ARRAY:");
+       for(int numbers: area){
+           System.out.print(numbers + " ");
+       }
 
 
 
